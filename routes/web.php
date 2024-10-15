fea402a92c1e846620ead6f8c2be69926f294ff1
Marcelo_2 <ci729165842022@jpardo.edu.pe>
2024-10-15 13:55:15 -0500

    Agregar pagina de Equipo

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 // Ruta para la página "Index"
 Route::get('/', function () {
     return view('index');
